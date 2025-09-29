dd33e24a11366ceb404cfb86fe621e036a1a9f0c
afsal-techon <techon.afsal@gmail.com>
2025-09-29 11:57:36 +0530

    ok set

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 
 const connectDB = async (req, res)=>{
     try{
-        const dbURI =  'mongodb://127.0.0.1:27017/EMP';
+        // const dbURI =  'mongodb://127.0.0.1:27017/EMP';
+        const dbURI =  'mongodb+srv://techonafsal_db_user:GcHvyjaH6EHj8fcx@cluster0.omxeyim.mongodb.net/EMP?retryWrites=true&w=majority&appName=Cluster0';
         await mongoose.connect(dbURI);
         console.log('Database connected succssfully')
 
